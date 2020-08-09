85c4a81d53dfd05e533848898c39ead4680c41db
lukechu1997 <lukechu421@gmail.com>
2020-08-09 17:17:53 +0800

    change readme

--- 03 - CSS Variables/app.js
+++ 03 - CSS Variables/app.js
@@ -2,8 +2,8 @@ const inputs = document.querySelectorAll('.controls input');
 
 function handelUpdate() {
     const suffix = this.dataset.sizing || '';
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix); //設定css屬性值(屬性名, 屬性值)
 }
 
-inputs.forEach(input => input.addEventListener('change', handelUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handelUpdate));
+inputs.forEach(input => input.addEventListener('change', handelUpdate)); //更改數值時觸發事件
+inputs.forEach(input => input.addEventListener('mousemove', handelUpdate)); //滑鼠移動時觸發事件
